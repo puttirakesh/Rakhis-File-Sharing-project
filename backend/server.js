a17ea5724b35f67afc66da8b3dfeffa707baa3c2
Putti Rakesh <puttirakesh99@gmail.com>
2025-09-08 20:30:12 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://chandan-kumar-file-sharing-platform-theta.vercel.app",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
