a82e18f6e45ccb35426d407c0854ab0a0e27af89
Putti Rakesh <puttirakesh99@gmail.com>
2025-09-08 21:07:59 +0530

    testa

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+import axios from "axios";
 
 app.use(express.json());
 
@@ -302,7 +303,7 @@ app.put("/api/topics/:topicId", auth, requireTeacher, async (req, res) => {
 });
 
 // Download File (stream from Cloudinary)
-import axios from "axios";
+
 
 app.get("/api/download/:fileId", auth, async (req, res) => {
   try {
